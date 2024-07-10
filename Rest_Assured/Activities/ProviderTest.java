b498864a36b2f7fad0ac6aa1bbf48b2c63d63201
Nandhini-Ponnraj <udhayanandhini.p@ibm.com>
2024-07-10 16:10:32 +0530

    Add files via upload

---
+++ Rest_Assured/Activities/ProviderTest.java
@@ -0,0 +1,25 @@
+package liveproject;
+
+import au.com.dius.pact.consumer.dsl.DslPart;
+import au.com.dius.pact.consumer.dsl.PactDslJsonArray;
+import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
+import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
+import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
+import au.com.dius.pact.core.model.RequestResponsePact;
+import au.com.dius.pact.core.model.annotations.Pact;
+import au.com.dius.pact.core.model.annotations.PactFolder;
+import au.com.dius.pact.provider.junit.Provider;
+import au.com.dius.pact.provider.junit.State;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Provider("UserProvider")
+@PactFolder("target/pacts")
+public class ProviderTest {
+    @State("POST request")
+    public void state1(){
+
+}
+}
