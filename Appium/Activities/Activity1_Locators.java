d567cfa1228e1b4a0ce46b50e84c8e8cde523c67
Nandhini-Ponnraj <udhayanandhini.p@ibm.com>
2024-07-23 23:56:33 +0530

    Add files via upload

---
+++ Appium/Activities/Activity1_Locators.java
@@ -0,0 +1,49 @@
+package Activities;
+package activities;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Activity1_Locators {
+    AndroidDriver driver;
+
+    @BeforeClass
+    public void setUp() throws MalformedURLException {
+        //Set Capabilities
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setPlatformName("android");
+        options.setAutomationName("UiAutomator2");
+        options.setAppPackage("com.miui.calculator");
+        options.setAppActivity(".cal.CalculatorActivity");//".cal.CalculatorActivity
+        options.noReset();
+
+        //Set serverUrl
+        URL serverURL = new URL("http://localhost:4723/wd/hub");
+
+        //Initialize driver
+        driver = new AndroidDriver(serverURL, options);
+    }
+
+    @Test
+    public void multiplyTest() {
+        driver.findElement(AppiumBy.id("btn_7_s")).click();
+        //Find  "*" and tap it
+        driver.findElement(AppiumBy.accessibilityId("Multiply")).click();
+        //Find number 5 and tap it
+        driver.findElement(AppiumBy.id("btn_5_s")).click();
+        //Find "=" and tap it
+        driver.findElement(AppiumBy.accessibilityId("equals")).click();
+        String result = driver.findElement(AppiumBy.id("result")).getText();
+
+        // Assertion
+        Assert.assertEquals(result, "= 35");
+    }
+}
+
